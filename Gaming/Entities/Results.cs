1cd01d1821a8683fcf5115360231f4557afec782
KirgizBala <112088974+KirgizBala@users.noreply.github.com>
2023-03-18 16:58:43 +0600

    Update Results.cs

--- Gaming/Entities/Results.cs
+++ Gaming/Entities/Results.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public Players Player { get; set; }
+        public int PlayerId { get; set; }
         public Games Game { get; set; }
+        public int GameId { get; set; }
         public DateTime Date { get; set; }
         public string Result { get; set; }
 
