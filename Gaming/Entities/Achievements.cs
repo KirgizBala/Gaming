187a6ba5b6eea579b8f18123e33d4671f6f9313a
KirgizBala <112088974+KirgizBala@users.noreply.github.com>
2023-03-18 16:58:39 +0600

    Update Achievements.cs

--- Gaming/Entities/Achievements.cs
+++ Gaming/Entities/Achievements.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public Players Player { get; set; }
+        public int PlayerId { get; set; }
         public string Desription { get; set; }
         public DateTime Date { get; set; }
 
